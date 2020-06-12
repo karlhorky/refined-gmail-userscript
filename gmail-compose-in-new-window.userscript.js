2dbcfd3c6dc28c45af8bf9eb99750a946d289bdb
Karl Horky <karl.horky@gmail.com>
2020-06-12 08:02:08 +0200

    Add first rudimentary version of Reply button

--- gmail-compose-in-new-window.userscript.js
+++ gmail-compose-in-new-window.userscript.js
@@ -1,3 +1,5 @@
+/* global globalThis */
+
 // ==UserScript==
 // @name         Gmail Compose in New Window
 // @namespace    https://www.karlhorky.com/
@@ -9,7 +11,8 @@
 // ==/UserScript==
 
 (async function () {
-  "use strict";
+  // eslint-disable-next-line strict
+  'use strict';
 
   function waitForElement(selector) {
     return new Promise((resolve, reject) => {
@@ -26,30 +29,110 @@
     });
   }
 
-  const composeButton = await waitForElement(".T-I.J-J5-Ji.T-I-KE.L3");
+  const clickInterceptorHtml =
+    '<div style="position:absolute; height:100%; width:100%;"></div>';
+
+  const composeButton = await waitForElement('.T-I.J-J5-Ji.T-I-KE.L3');
   if (composeButton) {
     // Add element to intercept click
-    composeButton.innerHTML =
-      'Compose<div style="position:absolute; height:100%; width:100%;"></div>';
+    composeButton.innerHTML += clickInterceptorHtml;
 
-    const clickInterceptor = composeButton.querySelector("div");
+    const clickInterceptor = composeButton.querySelector('div');
 
     // Use `pointerdown` to capture both touches and mouse clicks
-    clickInterceptor.addEventListener("pointerdown", (event) => {
+    clickInterceptor.addEventListener('pointerdown', (event) => {
       event.stopPropagation();
       event.preventDefault();
 
       // Use `mousedown` and `mouseup` instead of `click` for Gmail
       // Source: https://stackoverflow.com/a/57512820/1268612
       composeButton.dispatchEvent(
-        new MouseEvent("mousedown", { shiftKey: true })
+        new MouseEvent('mousedown', { shiftKey: true }),
       );
 
       setTimeout(() =>
         composeButton.dispatchEvent(
-          new MouseEvent("mouseup", { shiftKey: true })
-        )
+          new MouseEvent('mouseup', { shiftKey: true }),
+        ),
       );
     });
   }
+
+  const originalOpen = globalThis.XMLHttpRequest.prototype.open;
+
+  // TODO: Use mutation observer instead of XHR monkey patching
+  // to improve speed and reliability (handler not registered
+  // when Reply button is recreated after closing pop-up and
+  // trashing the old reply)
+  globalThis.XMLHttpRequest.prototype.open = function open(method, url) {
+    const isThreadViewRequest =
+      method === 'POST' &&
+      url
+        .split('/')
+        .pop()
+        .match(/s\?hl=en&c=\d+/);
+    if (!isThreadViewRequest) console.log(url);
+
+    if (isThreadViewRequest) {
+      const replyButton = document.querySelector('.Bu div.if .ams.bkH');
+      const forwardButton = document.querySelector('.Bu div.if .ams.bkG');
+
+      if (!replyButton.querySelector('div')) {
+        // TODO: Also check for visibility?
+        // document.querySelector('.Bu div.if .ams.bkH').offsetParent !== null
+        replyButton.innerHTML += clickInterceptorHtml;
+        replyButton.querySelector('div').addEventListener('click', (event) => {
+          event.stopPropagation();
+          event.preventDefault();
+
+          // Reply button does not accept normal `new MouseEvent()`
+          const options = {
+            pointerX: 0,
+            pointerY: 0,
+            button: 0,
+            ctrlKey: false,
+            altKey: false,
+            shiftKey: true,
+            metaKey: false,
+            bubbles: true,
+            cancelable: true,
+          };
+          const newEvent = document.createEvent('MouseEvents');
+          newEvent.initMouseEvent(
+            'click',
+            options.bubbles,
+            options.cancelable,
+            document.defaultView,
+            options.button,
+            options.pointerX,
+            options.pointerY,
+            options.pointerX,
+            options.pointerY,
+            options.ctrlKey,
+            options.altKey,
+            options.shiftKey,
+            options.metaKey,
+            options.button,
+            replyButton,
+          );
+
+          replyButton.dispatchEvent(newEvent);
+
+          // TODO: Use mutation observer instead of setTimeout
+          // to improve performance and reliability
+          setTimeout(() => {
+            document
+              .querySelector('img.Hq.aUG')
+              .dispatchEvent(new MouseEvent('mouseup', { shiftKey: true }));
+          }, 1500);
+        });
+      }
+      // Reply button
+      //console.log(document.querySelector('.Bu div.if .ams.bkH').offsetParent !== null);
+      // Forward button
+      //console.log(document.querySelector('.Bu div.if .ams.bkG').offsetParent !== null);
+    }
+
+    return originalOpen.apply(this, arguments);
+  };
 })();
