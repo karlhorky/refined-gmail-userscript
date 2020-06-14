ff0774eec7f52afbcd6e6e56595607c46b096470
Karl Horky <karl.horky@gmail.com>
2020-06-14 19:11:07 +0200

    Add first working version
    With:
    - Compose button
    - Reply / Reply All button at top of thread
    - Reply button at bottom of thread
    - Reply All button at bottom of thread
    - Forward button at bottom of thread
    
    Buggy, not yet complete:
    - Reply menu item in menu at top of thread
    - Reply All menu item in menu at top of thread
    - Forward menu item in menu at top of thread
    
    Closes #1

--- gmail-compose-in-new-window.userscript.js
+++ gmail-compose-in-new-window.userscript.js
@@ -1,5 +1,3 @@
-/* global globalThis */
-
 // ==UserScript==
 // @name         Gmail Compose in New Window
 // @namespace    https://www.karlhorky.com/
@@ -10,129 +8,265 @@
 // @grant        none
 // ==/UserScript==
 
-(async function () {
+(function () {
   // eslint-disable-next-line strict
   'use strict';
 
-  function waitForElement(selector) {
-    return new Promise((resolve, reject) => {
-      const interval = setInterval(checkForElementAndResolve, 300);
+  const eventMatchers = {
+    HTMLEvents: /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+    MouseEvents: /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/,
+  };
 
-      function checkForElementAndResolve() {
-        const element = document.querySelector(selector);
+  function dispatchDocumentEvent(element, eventName, optionsParam = {}) {
+    const options = {
+      pointerX: 0,
+      pointerY: 0,
+      button: 0,
+      ctrlKey: false,
+      altKey: false,
+      shiftKey: false,
+      metaKey: false,
+      bubbles: true,
+      cancelable: true,
+      ...optionsParam,
+    };
 
-        if (element) {
-          clearInterval(interval);
-          resolve(element);
-        }
+    let event;
+    let eventType = null;
+
+    for (const name in eventMatchers) {
+      if (eventMatchers[name].test(eventName)) {
+        eventType = name;
+        break;
       }
-    });
-  }
-
-  const clickInterceptorHtml =
-    '<div style="position:absolute; height:100%; width:100%;"></div>';
-
-  const composeButton = await waitForElement('.T-I.J-J5-Ji.T-I-KE.L3');
-  if (composeButton) {
-    // Add element to intercept click
-    composeButton.innerHTML += clickInterceptorHtml;
-
-    const clickInterceptor = composeButton.querySelector('div');
-
-    // Use `pointerdown` to capture both touches and mouse clicks
-    clickInterceptor.addEventListener('pointerdown', (event) => {
-      event.stopPropagation();
-      event.preventDefault();
-
-      // Use `mousedown` and `mouseup` instead of `click` for Gmail
-      // Source: https://stackoverflow.com/a/57512820/1268612
-      composeButton.dispatchEvent(
-        new MouseEvent('mousedown', { shiftKey: true }),
-      );
-
-      setTimeout(() =>
-        composeButton.dispatchEvent(
-          new MouseEvent('mouseup', { shiftKey: true }),
-        ),
-      );
-    });
-  }
-
-  const originalOpen = globalThis.XMLHttpRequest.prototype.open;
-
-  // TODO: Use mutation observer instead of XHR monkey patching
-  // to improve speed and reliability (handler not registered
-  // when Reply button is recreated after closing pop-up and
-  // trashing the old reply)
-  globalThis.XMLHttpRequest.prototype.open = function open(method, url) {
-    const isThreadViewRequest =
-      method === 'POST' &&
-      url
-        .split('/')
-        .pop()
-        .match(/s\?hl=en&c=\d+/);
-    if (!isThreadViewRequest) console.log(url);
-
-    if (isThreadViewRequest) {
-      const replyButton = document.querySelector('.Bu div.if .ams.bkH');
-      const forwardButton = document.querySelector('.Bu div.if .ams.bkG');
-
-      if (!replyButton.querySelector('div')) {
-        // TODO: Also check for visibility?
-        // document.querySelector('.Bu div.if .ams.bkH').offsetParent !== null
-        replyButton.innerHTML += clickInterceptorHtml;
-        replyButton.querySelector('div').addEventListener('click', (event) => {
-          event.stopPropagation();
-          event.preventDefault();
-
-          // Reply button does not accept normal `new MouseEvent()`
-          const options = {
-            pointerX: 0,
-            pointerY: 0,
-            button: 0,
-            ctrlKey: false,
-            altKey: false,
-            shiftKey: true,
-            metaKey: false,
-            bubbles: true,
-            cancelable: true,
-          };
-          const newEvent = document.createEvent('MouseEvents');
-          newEvent.initMouseEvent(
-            'click',
-            options.bubbles,
-            options.cancelable,
-            document.defaultView,
-            options.button,
-            options.pointerX,
-            options.pointerY,
-            options.pointerX,
-            options.pointerY,
-            options.ctrlKey,
-            options.altKey,
-            options.shiftKey,
-            options.metaKey,
-            options.button,
-            replyButton,
-          );
-
-          replyButton.dispatchEvent(newEvent);
-
-          // TODO: Use mutation observer instead of setTimeout
-          // to improve performance and reliability
-          setTimeout(() => {
-            document
-              .querySelector('img.Hq.aUG')
-              .dispatchEvent(new MouseEvent('mouseup', { shiftKey: true }));
-          }, 1500);
-        });
-      }
-      // Reply button
-      //console.log(document.querySelector('.Bu div.if .ams.bkH').offsetParent !== null);
-      // Forward button
-      //console.log(document.querySelector('.Bu div.if .ams.bkG').offsetParent !== null);
     }
 
-    return originalOpen.apply(this, arguments);
+    if (!eventType)
+      throw new SyntaxError(
+        'Only HTMLEvents and MouseEvents interfaces are supported',
+      );
+
+    event = document.createEvent(eventType);
+
+    if (eventType === 'HTMLEvents') {
+      event.initEvent(eventName, options.bubbles, options.cancelable);
+    } else {
+      event.initMouseEvent(
+        eventName,
+        options.bubbles,
+        options.cancelable,
+        document.defaultView,
+        options.button,
+        options.pointerX,
+        options.pointerY,
+        options.pointerX,
+        options.pointerY,
+        options.ctrlKey,
+        options.altKey,
+        options.shiftKey,
+        options.metaKey,
+        options.button,
+        element,
+      );
+    }
+    element.dispatchEvent(event);
+  }
+
+  const elementCallbacks = {};
+
+  // function waitForElement(selector) {
+  //   return new Promise((resolve, reject) => {
+  //     const interval = setInterval(checkForElementAndResolve, 300);
+
+  //     function checkForElementAndResolve() {
+  //       const element = document.querySelector(selector);
+
+  //       if (element) {
+  //         clearInterval(interval);
+  //         resolve(element);
+  //       }
+  //     }
+  //   });
+  // }
+
+  const clickInterceptorHtml =
+    '<div compose-in-new-window-interceptor style="position:absolute; z-index: 1000; height:100%; width:100%;"></div>';
+
+  elementCallbacks['.T-I.J-J5-Ji.T-I-KE.L3'] = (composeButton) => {
+    if (!composeButton.querySelector('[compose-in-new-window-interceptor]')) {
+      // Add element to intercept click
+      composeButton.innerHTML += clickInterceptorHtml;
+
+      const clickInterceptor = composeButton.querySelector(
+        '[compose-in-new-window-interceptor]',
+      );
+
+      // Use `pointerdown` to capture both touches and mouse clicks
+      clickInterceptor.addEventListener('pointerdown', (event) => {
+        event.stopPropagation();
+
+        // Use `mousedown` and `mouseup` instead of `click` for Gmail
+        // Source: https://stackoverflow.com/a/57512820/1268612
+        composeButton.dispatchEvent(
+          new MouseEvent('mousedown', { shiftKey: true }),
+        );
+
+        setTimeout(() =>
+          composeButton.dispatchEvent(
+            new MouseEvent('mouseup', { shiftKey: true }),
+          ),
+        );
+      });
+    }
   };
+
+  function addShiftToClickWithDocumentEvent(element) {
+    if (!element.querySelector('[compose-in-new-window-interceptor]')) {
+      element.innerHTML += clickInterceptorHtml;
+      element
+        .querySelector('[compose-in-new-window-interceptor]')
+        // Events other than `click` do not work here
+        .addEventListener('click', (event) => {
+          event.stopPropagation();
+          // - Reply button does not accept normal `new MouseEvent()`
+          // - Events other than `click` do not work here
+          dispatchDocumentEvent(element, 'click', {
+            shiftKey: true,
+          });
+        });
+    }
+  }
+
+  // Reply button at top of thread
+  elementCallbacks['.T-I.J-J5-Ji.T-I-Js-IF.aaq.T-I-ax7.L3'] = (
+    threadTopReplyButton,
+  ) => {
+    addShiftToClickWithDocumentEvent(threadTopReplyButton);
+  };
+
+  /*
+
+  FIXME: Buggy, first fix before using
+
+  function addShiftToMenuItemClickWithMouseEvent(
+    menuItemElement,
+    clickableElement,
+  ) {
+    if (!menuItemElement.querySelector('[compose-in-new-window-interceptor]')) {
+      // Add element to intercept click
+      menuItemElement.innerHTML += clickInterceptorHtml;
+
+      const clickInterceptor = menuItemElement.querySelector(
+        '[compose-in-new-window-interceptor]',
+      );
+
+      function handler(event) {
+        event.stopPropagation();
+
+        dispatchDocumentEvent(clickableElement, 'click', {
+          shiftKey: true,
+        });
+      }
+
+      // Both `mousedown` and `pointerdown` are required
+      // on menu items for whatever reason
+      clickInterceptor.addEventListener('mousedown', handler);
+      clickInterceptor.addEventListener('pointerdown', handler);
+    }
+  }
+
+  // Reply menu item at menu at top of thread
+  elementCallbacks['.b7.J-M > .J-N:nth-child(1) > .J-N-Jz'] = (
+    replyMenuItem,
+  ) => {
+    addShiftToMenuItemClickWithMouseEvent(
+      replyMenuItem,
+      document.querySelector('.Bu div.if .ams.bkH'),
+    );
+  };
+
+  // Reply All menu item at menu at top of thread
+  elementCallbacks['.b7.J-M > .J-N:nth-child(2) > .J-N-Jz'] = (
+    replyAllMenuItem,
+  ) => {
+    addShiftToMenuItemClickWithMouseEvent(
+      replyAllMenuItem,
+      document.querySelector('.Bu div.if .ams.bkI'),
+    );
+  };
+
+  // Forward menu item at menu at top of thread
+  elementCallbacks['.b7.J-M > .J-N:nth-child(3) > .J-N-Jz'] = (
+    forwardMenuItem,
+  ) => {
+    addShiftToMenuItemClickWithMouseEvent(
+      forwardMenuItem,
+      document.querySelector('.Bu div.if .ams.bkG'),
+    );
+  };
+  */
+
+  // Reply All button at bottom of thread
+  elementCallbacks['.Bu div.if .ams.bkI'] = (threadBottomReplyAllButton) => {
+    addShiftToClickWithDocumentEvent(threadBottomReplyAllButton);
+  };
+
+  // Reply button at bottom of thread
+  elementCallbacks['.Bu div.if .ams.bkH'] = (threadBottomReplyButton) => {
+    addShiftToClickWithDocumentEvent(threadBottomReplyButton);
+  };
+
+  // Forward button at bottom of thread
+  elementCallbacks['.Bu div.if .ams.bkG'] = (threadBottomForwardButton) => {
+    addShiftToClickWithDocumentEvent(threadBottomForwardButton);
+  };
+
+  // Click on the full screen / pop out button
+  // with the Shift key as soon as it appears
+  elementCallbacks['img.Hq.aUG'] = (fullScreenAndPopoutButton) => {
+    setTimeout(() => {
+      fullScreenAndPopoutButton.dispatchEvent(
+        new MouseEvent('mouseup', { shiftKey: true }),
+      );
+    }, 150);
+  };
+
+  const elementCallbacksSelectors = Object.keys(elementCallbacks);
+
+  const runElementCallbacksScheduled = (() => {
+    let scheduled = false;
+
+    return function runElementCallbacks() {
+      if (scheduled === true) return;
+
+      scheduled = true;
+      setTimeout(() => {
+        elementCallbacksSelectors.forEach((selector) => {
+          const element = document.querySelector(selector);
+
+          if (
+            // No element
+            !element
+            // Element not visible
+            // ||element.offsetParent === null
+          ) {
+            return;
+          }
+
+          elementCallbacks[selector](element);
+        });
+        scheduled = false;
+      }, 80);
+    };
+  })();
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(runElementCallbacksScheduled);
+
+  // Start observing the target node for configured mutations
+  observer.observe(document.body, {
+    attributes: true,
+    childList: true,
+    subtree: true,
+  });
 })();
