6a18651d7ce7ef384604cb54097630722ad8fa2c
Karl Horky <karl.horky@gmail.com>
2021-01-23 13:20:54 +0100

    Upgrade script to Violentmonkey format

--- refined-gmail.user.js
+++ refined-gmail.user.js
@@ -1,150 +1,146 @@
 // ==UserScript==
 // @name         Refined Gmail
-// @namespace    https://www.karlhorky.com/
-// @version      1.1
 // @description  Compose in New Windows, App Badge for Unread Emails
+// @version      1.1.0
 // @author       Karl Horky
+// @namespace    https://www.karlhorky.com/
 // @match        https://mail.google.com/mail/u/*
 // @grant        none
 // ==/UserScript==
 
-(function () {
-  // eslint-disable-next-line strict
-  'use strict';
+const eventMatchers = {
+  HTMLEvents: /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+  MouseEvents: /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/,
+};
 
-  const eventMatchers = {
-    HTMLEvents: /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
-    MouseEvents: /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/,
+function dispatchDocumentEvent(element, eventName, optionsParam = {}) {
+  const options = {
+    pointerX: 0,
+    pointerY: 0,
+    button: 0,
+    ctrlKey: false,
+    altKey: false,
+    shiftKey: false,
+    metaKey: false,
+    bubbles: true,
+    cancelable: true,
+    ...optionsParam,
   };
 
-  function dispatchDocumentEvent(element, eventName, optionsParam = {}) {
-    const options = {
-      pointerX: 0,
-      pointerY: 0,
-      button: 0,
-      ctrlKey: false,
-      altKey: false,
-      shiftKey: false,
-      metaKey: false,
-      bubbles: true,
-      cancelable: true,
-      ...optionsParam,
-    };
+  let event;
+  let eventType = null;
 
-    let event;
-    let eventType = null;
-
-    for (const name in eventMatchers) {
-      if (eventMatchers[name].test(eventName)) {
-        eventType = name;
-        break;
-      }
+  for (const name in eventMatchers) {
+    if (eventMatchers[name].test(eventName)) {
+      eventType = name;
+      break;
     }
-
-    if (!eventType)
-      throw new SyntaxError(
-        'Only HTMLEvents and MouseEvents interfaces are supported',
-      );
-
-    event = document.createEvent(eventType);
-
-    if (eventType === 'HTMLEvents') {
-      event.initEvent(eventName, options.bubbles, options.cancelable);
-    } else {
-      event.initMouseEvent(
-        eventName,
-        options.bubbles,
-        options.cancelable,
-        document.defaultView,
-        options.button,
-        options.pointerX,
-        options.pointerY,
-        options.pointerX,
-        options.pointerY,
-        options.ctrlKey,
-        options.altKey,
-        options.shiftKey,
-        options.metaKey,
-        options.button,
-        element,
-      );
-    }
-    element.dispatchEvent(event);
   }
 
-  const elementCallbacks = {};
+  if (!eventType)
+    throw new SyntaxError(
+      'Only HTMLEvents and MouseEvents interfaces are supported',
+    );
 
-  // function waitForElement(selector) {
-  //   return new Promise((resolve, reject) => {
-  //     const interval = setInterval(checkForElementAndResolve, 300);
+  event = document.createEvent(eventType);
 
-  //     function checkForElementAndResolve() {
-  //       const element = document.querySelector(selector);
+  if (eventType === 'HTMLEvents') {
+    event.initEvent(eventName, options.bubbles, options.cancelable);
+  } else {
+    event.initMouseEvent(
+      eventName,
+      options.bubbles,
+      options.cancelable,
+      document.defaultView,
+      options.button,
+      options.pointerX,
+      options.pointerY,
+      options.pointerX,
+      options.pointerY,
+      options.ctrlKey,
+      options.altKey,
+      options.shiftKey,
+      options.metaKey,
+      options.button,
+      element,
+    );
+  }
+  element.dispatchEvent(event);
+}
 
-  //       if (element) {
-  //         clearInterval(interval);
-  //         resolve(element);
-  //       }
-  //     }
-  //   });
-  // }
+const elementCallbacks = {};
 
-  const clickInterceptorHtml =
-    '<div compose-in-new-window-interceptor style="position:absolute; z-index: 1000; height:100%; width:100%;"></div>';
+// function waitForElement(selector) {
+//   return new Promise((resolve, reject) => {
+//     const interval = setInterval(checkForElementAndResolve, 300);
 
-  elementCallbacks['.T-I.J-J5-Ji.T-I-KE.L3'] = (composeButton) => {
-    if (!composeButton.querySelector('[compose-in-new-window-interceptor]')) {
-      // Add element to intercept click
-      composeButton.innerHTML += clickInterceptorHtml;
+//     function checkForElementAndResolve() {
+//       const element = document.querySelector(selector);
 
-      const clickInterceptor = composeButton.querySelector(
-        '[compose-in-new-window-interceptor]',
+//       if (element) {
+//         clearInterval(interval);
+//         resolve(element);
+//       }
+//     }
+//   });
+// }
+
+const clickInterceptorHtml =
+  '<div compose-in-new-window-interceptor style="position:absolute; z-index: 1000; height:100%; width:100%;"></div>';
+
+elementCallbacks['.T-I.J-J5-Ji.T-I-KE.L3'] = (composeButton) => {
+  if (!composeButton.querySelector('[compose-in-new-window-interceptor]')) {
+    // Add element to intercept click
+    composeButton.innerHTML += clickInterceptorHtml;
+
+    const clickInterceptor = composeButton.querySelector(
+      '[compose-in-new-window-interceptor]',
+    );
+
+    // Use `pointerdown` to capture both touches and mouse clicks
+    clickInterceptor.addEventListener('pointerdown', (event) => {
+      event.stopPropagation();
+
+      // Use `mousedown` and `mouseup` instead of `click` for Gmail
+      // Source: https://stackoverflow.com/a/57512820/1268612
+      composeButton.dispatchEvent(
+        new MouseEvent('mousedown', { shiftKey: true }),
       );
 
-      // Use `pointerdown` to capture both touches and mouse clicks
-      clickInterceptor.addEventListener('pointerdown', (event) => {
-        event.stopPropagation();
-
-        // Use `mousedown` and `mouseup` instead of `click` for Gmail
-        // Source: https://stackoverflow.com/a/57512820/1268612
+      setTimeout(() =>
         composeButton.dispatchEvent(
-          new MouseEvent('mousedown', { shiftKey: true }),
-        );
-
-        setTimeout(() =>
-          composeButton.dispatchEvent(
-            new MouseEvent('mouseup', { shiftKey: true }),
-          ),
-        );
-      });
-    }
-  };
-
-  function addShiftToClickWithDocumentEvent(element) {
-    if (!element.querySelector('[compose-in-new-window-interceptor]')) {
-      element.innerHTML += clickInterceptorHtml;
-      element
-        .querySelector('[compose-in-new-window-interceptor]')
-        // Events other than `click` do not work here
-        .addEventListener('click', (event) => {
-          event.stopPropagation();
-          // - Reply button does not accept normal `new MouseEvent()`
-          // - Events other than `click` do not work here
-          dispatchDocumentEvent(element, 'click', {
-            shiftKey: true,
-          });
-        });
-    }
+          new MouseEvent('mouseup', { shiftKey: true }),
+        ),
+      );
+    });
   }
+};
 
-  // Reply button at top of thread
-  elementCallbacks['.T-I.J-J5-Ji.T-I-Js-IF.aaq.T-I-ax7.L3'] = (
-    threadTopReplyButton,
-  ) => {
-    addShiftToClickWithDocumentEvent(threadTopReplyButton);
-  };
+function addShiftToClickWithDocumentEvent(element) {
+  if (!element.querySelector('[compose-in-new-window-interceptor]')) {
+    element.innerHTML += clickInterceptorHtml;
+    element
+      .querySelector('[compose-in-new-window-interceptor]')
+      // Events other than `click` do not work here
+      .addEventListener('click', (event) => {
+        event.stopPropagation();
+        // - Reply button does not accept normal `new MouseEvent()`
+        // - Events other than `click` do not work here
+        dispatchDocumentEvent(element, 'click', {
+          shiftKey: true,
+        });
+      });
+  }
+}
 
-  /*
+// Reply button at top of thread
+elementCallbacks['.T-I.J-J5-Ji.T-I-Js-IF.aaq.T-I-ax7.L3'] = (
+  threadTopReplyButton,
+) => {
+  addShiftToClickWithDocumentEvent(threadTopReplyButton);
+};
+
+/*
   FIXME: Buggy, first fix before using
   function addShiftToMenuItemClickWithMouseEvent(
     menuItemElement,
@@ -197,96 +193,95 @@
   };
   */
 
-  // Reply All button at bottom of thread
-  elementCallbacks['.Bu div.if .ams.bkI'] = (threadBottomReplyAllButton) => {
-    addShiftToClickWithDocumentEvent(threadBottomReplyAllButton);
-  };
+// Reply All button at bottom of thread
+elementCallbacks['.Bu div.if .ams.bkI'] = (threadBottomReplyAllButton) => {
+  addShiftToClickWithDocumentEvent(threadBottomReplyAllButton);
+};
 
-  // Reply button at bottom of thread
-  elementCallbacks['.Bu div.if .ams.bkH'] = (threadBottomReplyButton) => {
-    addShiftToClickWithDocumentEvent(threadBottomReplyButton);
-  };
+// Reply button at bottom of thread
+elementCallbacks['.Bu div.if .ams.bkH'] = (threadBottomReplyButton) => {
+  addShiftToClickWithDocumentEvent(threadBottomReplyButton);
+};
 
-  // Forward button at bottom of thread
-  elementCallbacks['.Bu div.if .ams.bkG'] = (threadBottomForwardButton) => {
-    addShiftToClickWithDocumentEvent(threadBottomForwardButton);
-  };
+// Forward button at bottom of thread
+elementCallbacks['.Bu div.if .ams.bkG'] = (threadBottomForwardButton) => {
+  addShiftToClickWithDocumentEvent(threadBottomForwardButton);
+};
 
-  // Click on the full screen / pop out button
-  // with the Shift key as soon as it appears
-  elementCallbacks['img.Hq.aUG'] = (fullScreenAndPopoutButton) => {
-    setTimeout(() => {
-      fullScreenAndPopoutButton.dispatchEvent(
-        new MouseEvent('mouseup', { shiftKey: true }),
-      );
-    }, 150);
-  };
+// Click on the full screen / pop out button
+// with the Shift key as soon as it appears
+elementCallbacks['img.Hq.aUG'] = (fullScreenAndPopoutButton) => {
+  setTimeout(() => {
+    fullScreenAndPopoutButton.dispatchEvent(
+      new MouseEvent('mouseup', { shiftKey: true }),
+    );
+  }, 150);
+};
 
-  // Set the app badge with the number of unread emails in inbox
-  //
-  // https://web.dev/badging-api/
-  //
-  // To make this work with multiple Gmail accounts, create desktop apps
-  // using the web app manifest trick documented at the link below.
-  //
-  // You will want to use a Gmail URL with the slash at the end, such as:
-  // https://mail.google.com/mail/u/0/
-  //
-  // https://github.com/karlhorky/dotfiles/blob/master/tricks/gsuite-google-calendar-chrome-desktop-app-shortcut.md
-  elementCallbacks['.aim:first-child .aio.UKr6le'] = (inboxRowLeftMenu) => {
-    const unreadEmailsCountContainer = inboxRowLeftMenu.querySelector('.bsU');
+// Set the app badge with the number of unread emails in inbox
+//
+// https://web.dev/badging-api/
+//
+// To make this work with multiple Gmail accounts, create desktop apps
+// using the web app manifest trick documented at the link below.
+//
+// You will want to use a Gmail URL with the slash at the end, such as:
+// https://mail.google.com/mail/u/0/
+//
+// https://github.com/karlhorky/dotfiles/blob/master/tricks/gsuite-google-calendar-chrome-desktop-app-shortcut.md
+elementCallbacks['.aim:first-child .aio.UKr6le'] = (inboxRowLeftMenu) => {
+  const unreadEmailsCountContainer = inboxRowLeftMenu.querySelector('.bsU');
 
-    if (!unreadEmailsCountContainer) {
-      navigator.clearAppBadge().catch((err) => {
-        console.error('Error: Failed to clear app badge!');
-        console.error(err);
-      });
-      return;
-    }
-
-    const unreadEmailsCount = unreadEmailsCountContainer.innerText;
-    navigator.setAppBadge(unreadEmailsCount).catch((err) => {
-      console.error('Error: Failed to set app badge!');
+  if (!unreadEmailsCountContainer) {
+    navigator.clearAppBadge().catch((err) => {
+      console.error('Error: Failed to clear app badge!');
       console.error(err);
     });
-  };
+    return;
+  }
 
-  const elementCallbacksSelectors = Object.keys(elementCallbacks);
-
-  const runElementCallbacksScheduled = (() => {
-    let scheduled = false;
-
-    return function runElementCallbacks() {
-      if (scheduled === true) return;
-
-      scheduled = true;
-      setTimeout(() => {
-        elementCallbacksSelectors.forEach((selector) => {
-          const element = document.querySelector(selector);
-
-          if (
-            // No element
-            !element
-            // Element not visible
-            // ||element.offsetParent === null
-          ) {
-            return;
-          }
-
-          elementCallbacks[selector](element);
-        });
-        scheduled = false;
-      }, 80);
-    };
-  })();
-
-  // Create an observer instance linked to the callback function
-  const observer = new MutationObserver(runElementCallbacksScheduled);
-
-  // Start observing the target node for configured mutations
-  observer.observe(document.body, {
-    attributes: true,
-    childList: true,
-    subtree: true,
+  const unreadEmailsCount = unreadEmailsCountContainer.innerText;
+  navigator.setAppBadge(unreadEmailsCount).catch((err) => {
+    console.error('Error: Failed to set app badge!');
+    console.error(err);
   });
+};
+
+const elementCallbacksSelectors = Object.keys(elementCallbacks);
+
+const runElementCallbacksScheduled = (() => {
+  let scheduled = false;
+
+  return function runElementCallbacks() {
+    if (scheduled === true) return;
+
+    scheduled = true;
+    setTimeout(() => {
+      elementCallbacksSelectors.forEach((selector) => {
+        const element = document.querySelector(selector);
+
+        if (
+          // No element
+          !element
+          // Element not visible
+          // ||element.offsetParent === null
+        ) {
+          return;
+        }
+
+        elementCallbacks[selector](element);
+      });
+      scheduled = false;
+    }, 80);
+  };
 })();
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(runElementCallbacksScheduled);
+
+// Start observing the target node for configured mutations
+observer.observe(document.body, {
+  attributes: true,
+  childList: true,
+  subtree: true,
+});
